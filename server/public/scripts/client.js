bc648c62167126e8137e54abf7b691e6bd8e0bd0
sasham43 <sashasemail@gmail.com>
2016-11-25 22:48:05 -0600

    authentication service working, bit ugly

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,7 @@
 angular.module('PodcastApp', ['ui.router']);
 
 angular.module('PodcastApp').run(function($rootScope) {
-  $rootScope.$on("$stateChangeError", console.log.bind(console));
+  $rootScope.$on("$stateChangeError", console.log.bind(console)); // no idea if this is working
 });
 
 angular.module('PodcastApp').config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider){
@@ -13,6 +13,7 @@ angular.module('PodcastApp').config(['$stateProvider', '$locationProvider', func
   .state({
     name: 'login',
     url: '/login',
+    controller: 'LoginController',
     templateUrl: 'views/login.html'
   })
   .state({
@@ -32,15 +33,38 @@ angular.module('PodcastApp').config(['$stateProvider', '$locationProvider', func
 }]);
 
 angular.module('PodcastApp').factory('AuthCheckService', ['$http', '$location', '$state', function($http, $location, $state){
+  var user = {};
+  var authenticated = false;
+
   return {
+    user: user,
     authCheck: function(){
-      $http.get('/auth/check').then(function(resp){
-        console.log('auth check resp:', resp);
-        return resp.data;
-      }, function(err){
-        console.log('auth check fail:', err);
-        $state.go('splash');
-      });
+      if (authenticated){
+        return authenticated;
+      } else {
+        $http.get('/auth/check')
+        .then(function(resp){
+          console.log('auth check resp:', resp);
+          authenticated = true;
+          return true;
+        }, function(err){
+          console.log('auth check fail:', err);
+          // $state.go('splash');
+          return false;
+        });
+      }
+    },
+    getUser: function(){
+      if(user.user != null){
+        return {user:user};
+      } else {
+        $http.get('/auth/check').then(function(resp){
+          user = resp.data;
+          return {user:resp.data};
+        }, function(err){
+          return err;
+        });
+      }
     }
   }
 }]);
@@ -50,16 +74,22 @@ angular.module('PodcastApp').controller('SplashController', ['$http', 'AuthCheck
   // AuthCheckService.authCheck();
 }]);
 
-angular.module('PodcastApp').controller('LoginController', ['$http', function($http){
+angular.module('PodcastApp').controller('LoginController', ['$http', 'AuthCheckService','$state', function($http, AuthCheckService, $state){
   console.log('Login controller loaded. ');
+  if(AuthCheckService.authCheck()){
+    $state.go('user');
+  }
 }]);
 
 angular.module('PodcastApp').controller('AboutController', ['$http', function($http){
   console.log('About controller loaded. ');
 }]);
 
-angular.module('PodcastApp').controller('UserController', ['$http', 'AuthCheckService', function($http, AuthCheckService){
+angular.module('PodcastApp').controller('UserController', ['$http', 'AuthCheckService', '$state', function($http, AuthCheckService, $state){
   var uc = this;
+  if(!AuthCheckService.authCheck()){
+    $state.go('login');
+  }
 
-  uc.user = AuthCheckService.authCheck();
+  uc.user = AuthCheckService.getUser();
 }]);
